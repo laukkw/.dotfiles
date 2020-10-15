ebefa0b291d6653b020181f00de07de153b608fc
rzry <1319339638@qq.com>
2020-10-15 21:14:34 +0800

    增加两个alias

--- zsh/.config/zsh/snippets/alias.zsh
+++ zsh/.config/zsh/snippets/alias.zsh
@@ -3,7 +3,9 @@ alias rm='rm -i'   rd='rmdir'   md='mkdir -p'
 alias ls='exa -bh'  la='ls -la'  lt='ls --tree'  ll='ls -l'  l='ls'
 alias dfh='df -h'  dus='du -sh' del='gio trash' dusa='dus --apparent-size'
 alias reload="exec $SHELL -l -i"  grep="command grep --colour=auto --binary-files=without-match --directories=skip"
+alias sfs='sshfs' fsmt='fusermount -zu'
 alias df='df -h'  du='du -h'  plast="last -20"
+alias ec='emacsclient'
 alias cp='cp -v'   mv='mv -v'
 
 load_localhost_addr() {
